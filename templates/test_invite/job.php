5d341dda744471c7fc5be1f215692b9d3514c8c7
Mike Haiduk <x29a100@gmail.com>
2024-11-21 21:59:03 +0100

    token list and bugfixes

--- templates/test_invite/job.php
+++ templates/test_invite/job.php
@@ -1,7 +1,7 @@
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . "/mfm-mail/utils.php";
 
-callLimitSec(60);
+//callLimitSec(60);
 
 $base_path = $_SERVER["DOCUMENT_ROOT"] . "/mfm-mail/ext/page3.json";
 $page = file_get_contents($base_path);
@@ -14,8 +14,6 @@ $langs = json_decode($langs, true);
 foreach ($page as &$item) {
     if (!isset($item[requested]) && $item[email] != null && $item[email] != "-1") {
         requestEquals("/mfm-mail/templates/test_invite/send.php", [
-            subject => "Testinvite",
-            utm_campaign => "test_invite1",
             username => $item[title],
             email => $item[email],
             lang => $langs[$item[localtion]],
--- templates/test_invite/send.php
+++ templates/test_invite/send.php
@@ -1,14 +1,23 @@
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . "/mfm-mail/utils.php";
 
-$subject = get_required(subject);
 $username = get_required(username);
 $email = get_required(email);
 $lang = get_required(lang, "index");
-$utm_campaign = get_required(utm_campaign);
+$utm_campaign = get_required(utm_campaign, date("m.d.y"));
 $site_domain = get_string(site_domain, "mytoken.space");
 
 $template = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/mfm-mail/templates/test_invite/$lang.html");
+
+$lines = explode("\n", $template);
+foreach ($lines as $line) {
+    $str = str_replace("<p class=\"subject\">", "", $line);
+    if ($str != $line) {
+        $str = str_replace("</p>", "", $str);
+        $subject = trim($str);
+        break;
+    }
+}
 
 $mail_hash = md5($email);
 $link_params = [
